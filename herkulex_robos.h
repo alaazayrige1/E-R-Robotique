532f5311f36ff12b11565d383eadef5817d592c9
alige1 <56106685+alige1@users.noreply.github.com>
2021-03-11 09:35:45 +0100

    Update and rename README.md to herkulex_robos.h

---
+++ herkulex_robos.h
@@ -0,0 +1,14 @@
+#ifndef HERKULEX_ROBOS_H
+#define HERKULEX_ROBOS_H
+
+
+#include "herkulex.h"
+
+extern char angle;
+
+void servo_init();
+void bras_droit();
+void bras_droite();
+void bras_gauche();
+
+#endif
