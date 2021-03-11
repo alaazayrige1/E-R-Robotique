899d98ed72de08c2107c5b6271437ea3610b515c
alaazayrige1 <56106756+alaazayrige1@users.noreply.github.com>
2021-03-11 09:55:38 +0100

    Update herkulex_robos.h

--- herkulex_robos.h
+++ herkulex_robos.h
@@ -1,12 +1,11 @@
 #ifndef HERKULEX_ROBOS_H
 #define HERKULEX_ROBOS_H
 
-
+#include "Robot_total.h"
 #include "herkulex.h"
 
 extern char angle;
 
-void servo_init();
 void bras_droit();
 void bras_droite();
 void bras_gauche();
