36b007272739f4f933c986d2be1b454633a945d6
alige1 <56106685+alige1@users.noreply.github.com>
2021-03-11 09:37:29 +0100

    Update and rename main.cpp to herkulex_robos.cpp

---
+++ herkulex_robos.cpp
@@ -0,0 +1,65 @@
+#include "herkulex_robos.h"
+
+char angle=0;
+
+Herkulex sv(D1,D0, 115200);
+
+
+
+void servo_init()
+{
+
+    sv.setTorque(1, TORQUE_ON);
+    sv.setTorque(2, TORQUE_ON);
+    sv.setTorque(3, TORQUE_ON);
+    sv.setTorque(4, TORQUE_ON);
+   
+    
+}
+
+void bras_droit()
+{
+    while(1)
+    {
+        if(angle==0)
+        {
+            sv.positionControl(1, 370, 50, GLED_ON);                                      sv.positionControl(2, 950, 50, GLED_ON);
+            sv.positionControl(3, 21, 50, GLED_ON);                                       sv.positionControl(4, 1000, 50, GLED_ON);
+            angle=1;
+        }
+        ThisThread::sleep_for(3000);
+    }   
+}
+
+void bras_droite()
+{
+    while(1)
+    {
+        if(angle==1)
+        {
+            sv.positionControl(1, 650, 50, GLED_ON);
+            sv.positionControl(2, 950, 50, GLED_ON);
+            sv.positionControl(3, 330, 50, GLED_ON);                                      
+            sv.positionControl(4, 1000, 50, GLED_ON);
+            angle=2;
+        }
+    ThisThread::sleep_for(3000);  
+    }
+}
+
+void bras_gauche()
+{
+    while(1)
+    {
+    if(angle==2)
+        {
+            sv.positionControl(1, 350, 50, GLED_ON);
+            sv.positionControl(2, 720, 50, GLED_ON);
+            sv.positionControl(3, 50, 50, GLED_ON); 
+            sv.positionControl(4, 21, 50, GLED_ON);
+            angle=0;
+        }
+    ThisThread::sleep_for(3000);
+    } 
+}
+
