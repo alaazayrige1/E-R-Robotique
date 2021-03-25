43ef16d387deaece43315ba1f7ec30bf7f1346d0
alaazayrige1 <56106756+alaazayrige1@users.noreply.github.com>
2021-03-25 16:58:25 +0100

    Create PointeurLaser.h

---
+++ PointeurLaser.h
@@ -0,0 +1,24 @@
+#ifndef POINTEURLASER_H
+#define POINTEURLASER_H
+
+#include "mbed.h"
+
+#define valeurlimite 10
+
+extern AnalogIn LaserUn;
+extern AnalogIn LaserDeux;
+extern AnalogIn LaserTrois;
+extern AnalogIn LaserQuatre;
+
+extern char position_laser_un;
+extern char position_laser_deux;
+extern char position_laser_trois;
+extern char position_laser_quatre;
+extern char laserun_proche;
+extern char laserdeux_proche;
+extern char lasertrois_proche;
+extern char laserquatre_proche;
+
+
+
+#endif
